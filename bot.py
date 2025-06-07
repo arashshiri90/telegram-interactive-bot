b4ce306f70ffcee8a3196584d9d83091868c112f
arash <44205047+arashshiri90@users.noreply.github.com>
2025-06-07 15:38:07 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
@@ -6,13 +5,23 @@ from aiogram.enums.parse_mode import ParseMode
 from handlers import router
 from dotenv import load_dotenv
 import os
+import threading
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 load_dotenv()
-
 TOKEN = os.getenv("BOT_TOKEN")
+
+# سرور ساختگی برای Render
+def dummy_http_server():
+    PORT = 8080
+    server = HTTPServer(("0.0.0.0", PORT), SimpleHTTPRequestHandler)
+    print(f"Dummy server running on port {PORT}")
+    server.serve_forever()
 
 async def main():
     bot = Bot(token=TOKEN, parse_mode=ParseMode.MARKDOWN)
+    await bot.delete_webhook(drop_pending_updates=True)
+
     dp = Dispatcher()
     dp.include_router(router)
 
@@ -20,4 +29,9 @@ async def main():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
+
+    # اجرای سرور ساختگی در یک Thread جدا
+    threading.Thread(target=dummy_http_server, daemon=True).start()
+
+    # اجرای ربات Aiogram
     asyncio.run(main())
